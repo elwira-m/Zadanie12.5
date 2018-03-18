e773a02d4817c1a71c0503f489239d6bac3063e6
elwira-m <36117564+elwira-m@users.noreply.github.com>
2018-03-18 21:56:44 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+$(function(){
+	
+  var prefix = "https://cors-anywhere.herokuapp.com/";
+  var tweetLink = "https://twitter.com/intent/tweet?text=";
+  var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";	
+	
+        /*
+        $.ajax({
+            dataType: "json",
+            url: quoteUrl,
+            data: null,
+            success: createTweet
+        });					                    		rownoznaczne z:
+        */
+
+  function getQuote() {
+     $.getJSON(prefix + quoteUrl, createTweet);
+      $.ajaxSetup({ cache: false });
+  }	
+	
+  function createTweet(input) {
+      var data = input[0];
+
+      var quoteText = $(data.content).text().trim();
+      var quoteAuthor = data.title;
+
+      if (!quoteAuthor.length) {
+         quoteAuthor = "Unknown author";
+       }
+      var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+      if (tweetText.length > 140) {	                                            	//jesli tweet bedzie 
+    	  getQuote();				                                                        //dluzszy, pobierze nastepny
+	    } else {
+	  	  var tweet = tweetLink + encodeURIComponent(tweetText);
+	 	    $('.quote').text(quoteText);
+    	  $('.author').text("Author: " + quoteAuthor);
+    	  $('.tweet').attr('href', tweet);
+	   }
+  }
+
+                                          //tworzy tweeta po wejsciu na strone i po kliknieciu przycisku
+  $(document).ready(function() {
+     getQuote();
+     $('.trigger').click(function() {
+        getQuote();
+     })
+  });
+
+})
