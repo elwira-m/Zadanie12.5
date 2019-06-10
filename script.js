eb7b1d13be4d5f7bec33bc392d07119f3e6a507e
Elwira Marcinkowska <elwira.marcinkowska@hotmail.com>
2019-06-10 03:12:19 +0200

    change style, add image

--- script.js
+++ script.js
@@ -1,49 +1,44 @@
+////////////////////////Random quote machine///////////////////
+
 $(function(){
 	
-  var prefix = "https://cors-anywhere.herokuapp.com/";
-  var tweetLink = "https://twitter.com/intent/tweet?text=";
-  var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";	
+    var prefix = "https://cors-anywhere.herokuapp.com/";
+    var tweetLink = "https://twitter.com/intent/tweet?text=";
+    var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";	
 	
-        /*
-        $.ajax({
-            dataType: "json",
-            url: quoteUrl,
-            data: null,
-            success: createTweet
-        });					                    		rownoznaczne z:
-        */
-
-  function getQuote() {
-     $.getJSON(prefix + quoteUrl, createTweet);
-      $.ajaxSetup({ cache: false });
-  }	
+       
+    function getQuote() {
+        $.getJSON(prefix + quoteUrl, createTweet);
+        $.ajaxSetup({ cache: false });
+    }	
 	
-  function createTweet(input) {
-      var data = input[0];
+    function createTweet(input) {
 
-      var quoteText = $(data.content).text().trim();
-      var quoteAuthor = data.title;
+        var data = input[0];
+        var quoteText = $(data.content).text().trim();
+        var quoteAuthor = data.title;
 
-      if (!quoteAuthor.length) {
-         quoteAuthor = "Unknown author";
-       }
-      var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
-      if (tweetText.length > 140) {	                                            	//jesli tweet bedzie 
-    	  getQuote();				                                                        //dluzszy, pobierze nastepny
+        if (!quoteAuthor.length) {
+            quoteAuthor = "Unknown author";
+        }
+
+        var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+        if (tweetText.length > 280) {	                                            	
+    	  getQuote();				                                                        
 	    } else {
-	  	  var tweet = tweetLink + encodeURIComponent(tweetText);
+	  	    var tweet = tweetLink + encodeURIComponent(tweetText);
 	 	    $('.quote').text(quoteText);
-    	  $('.author').text("Author: " + quoteAuthor);
-    	  $('.tweet').attr('href', tweet);
-	   }
-  }
-
-                                          //tworzy tweeta po wejsciu na strone i po kliknieciu przycisku
-  $(document).ready(function() {
-     getQuote();
-     $('.trigger').click(function() {
+    	    $('.author').text("Author: " + quoteAuthor);
+    	    $('.tweet').attr('href', tweet);
+	    };
+    };
+                                         
+    $(document).ready(function() {
         getQuote();
-     })
-  });
+        $('.trigger').click(function() {
+            getQuote();
+        });
+    });
 
-})
+});
