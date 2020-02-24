56cee14e68dacce61f8c651a28088f442a711ec2
elwira-m <36117564+elwira-m@users.noreply.github.com>
2020-02-24 01:32:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ $(function(){
 	
     var prefix = "https://cors-anywhere.herokuapp.com/";
     var tweetLink = "https://twitter.com/intent/tweet?text=";
-    /* var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1"; */
     var quoteUrl = "https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand";	
 	
        
@@ -16,8 +15,8 @@ $(function(){
     function createTweet(input) {
 
         var data = input[0];
-        var quoteText = $(data.content).text().trim();
-        var quoteAuthor = data.title;
+        var quoteText = $(data.content.rendered).text().trim();
+        var quoteAuthor = data.title.rendered;
 
         if (!quoteAuthor.length) {
             quoteAuthor = "Unknown author";
@@ -28,10 +27,10 @@ $(function(){
         if (tweetText.length > 280) {	                                            	
     	  getQuote();				                                                        
 	    } else {
-	  	    var tweet = tweetLink + encodeURIComponent(tweetText);
-	 	    $('.quote').text(quoteText);
-    	    $('.author').text("Author: " + quoteAuthor);
-    	    $('.tweet').attr('href', tweet);
+	  	 var tweet = tweetLink + encodeURIComponent(tweetText);
+	 	 $('.quote').text(quoteText);
+    	   	 $('.author').text("Author: " + quoteAuthor);
+    	   	 $('.tweet').attr('href', tweet);
 	    };
     };
                                          
